65cf15f3fc268ee69cf0379934cfd924c54fe685
Simon Kong <simon.kong95@gmail.com>
2018-12-16 02:54:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,7 +8,7 @@ end
 
 def monopoly
 	monopoly = {
-    railroads:{}
+    railroads:
   }
 
 end
@@ -18,7 +18,7 @@ def monopoly_with_second_tier
   monopoly = {
     railroads:{
       pieces:4
-      names:{}
+      names:
     }
   }
 end
